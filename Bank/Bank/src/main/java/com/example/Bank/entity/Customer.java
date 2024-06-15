0b25d04d113abd1cd68ceabfc40e0949ad920122
nisha710 <nisharekhawar@gmail.com>
2024-06-15 17:48:14 +0530

    Added all pervious Bank files

---
+++ Bank/Bank/src/main/java/com/example/Bank/entity/Customer.java
@@ -0,0 +1,12 @@
+package com.example.Bank.entity;
+
+import java.util.ArrayList;
+
+public class Customer {
+    int cusId;
+    String name;
+    double balance;
+    ArrayList<Integer> transactions;
+
+
+}
